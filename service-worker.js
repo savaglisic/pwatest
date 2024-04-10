5520616044185995f73a8bc42ebea15ee2f677e5
Sava Glisic <104223515+savaglisic@users.noreply.github.com>
2024-04-10 06:24:04 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './static/js/bundle.js', // You might need to adjust this depending on your build output
+  './static/js/0.chunk.js', // Include other generated JS files
+  './static/js/main.chunk.js', // Include other generated JS files
+  // Add other resources that you want to cache
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = ['v1'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
